50d7602d220bded519a705618c7b954972308d57
DESKTOP-O066MQR\Admin <rps53009@hotmail.com>
2021-01-17 11:29:26 +0700

    add workshop 3 part 2 17/1/2021

---
+++ set/access.py
@@ -0,0 +1,10 @@
+# iterate เป็นการใช้ลูปมาช่วย
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+    print(x)
+
+
+fruits = {"apple", "banana", "cherry"}
+if "apple" in fruits:
+    print("Yes, apple is a fruit")
---
+++ tuple/unplack.py
@@ -0,0 +1,7 @@
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits  # กระจายตัวแปรไปเก็บที่ละตัวตามลำดับของ tuple
+
+print(green)
+print(yellow)
+print(red)
